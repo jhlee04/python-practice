4230f9871681e7cecab285046c611f92173c8b71
jhlee <leejhk@kaist.ac.kr>
2016-02-16 20:03:58 +0900

    first commit

---
+++ read_passwd.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+infile = open("/etc/passwd", 'r') 
+lines =  infile.readlines()
+info={}  #  declare empty dictionary
+
+for line  in lines:
+    item=line.split(":")
+    
+    if int(item[2]) > 500 and int(item[2])<600:
+        info[item[0]]=[]
+	info[item[0]].append(item[5])
+print '** user : home directory **'
+for x in info : 
+     print x,':',info[x][0]
+#    for y in info[x]:
+#        print(x,':',info[x][y])
+
+infile.close()
+
+
