48e8cbf71d7f1d39aa44972035387643a7bc527f
jhlee <leejhk@kaist.ac.kr>
2016-02-17 20:45:49 +0900

    edit

--- read_passwd.py
+++ read_passwd.py
@@ -1,18 +1,35 @@
 #!/usr/bin/env python
+from pprint import pprint 
 
 infile = open("/etc/passwd", 'r') 
 lines =  infile.readlines()
-info={}  #  declare empty dictionary
+passwd = { }  #  declare empty dictionary
+dir_name = ('/home','/home2','/users/nst551')
+home = [] 
+home2 = [] 
+users = [] 
+
+
+for dir in dir_name : 
+    passwd[dir] = []  
 
 for line  in lines:
     item=line.split(":")
-    
-    if int(item[2]) > 500 and int(item[2])<600:
-        info[item[0]]=[]
-	info[item[0]].append(item[5])
-print '** user : home directory **'
-for x in info : 
-     print x,':',info[x][0]
+
+    if int(item[2]) > 500 :
+        dir = item[5].split("/")[1]
+        if dir == 'home' :
+            home.append(item[0]) 
+        elif dir == 'home2' :
+            home2.append(item[0]) 
+        elif dir == 'users' :
+            users.append(item[0]) 
+
+passwd['/home'].append(home)
+passwd['/home2'].append(home2)
+passwd['/users/nst551'].append(users)
+
+pprint(passwd)  
 
 infile.close()
 
