45018e8d4425fa9a71ab0a714de9375b7ae10264
jhlee04 <jhlee9104@kaist.ac.kr>
2016-03-09 15:26:14 +0900

    deleted and added files

--- qnmsg.py
+++ qnmsg.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import subprocess  
 from pprint import pprint 
 import re 
 
@@ -27,3 +28,18 @@ def passwd():
     infile.close()
     return passwd 
     
+def df(): 
+    cmd = "df -B G" 
+    f= subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE) 
+    f.stdout.readline()
+    size = {} 
+    for line in f.stdout.readlines():
+        size [ line.split()[-1] ] = line.split()[1][:-1]  
+    return size 
+
+def du(usrdir):
+    cmd = "du -sBG" + " " + usrdir 
+    f= subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE) 
+    usage = f.stdout.readline().split()[0][:-1] 
+    return usage
+
---
+++ run.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+from test_passwd import passwd  
+from pprint import pprint 
+
+pprint(passwd())  
